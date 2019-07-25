50596606f6d163a5c198dd42a8ab309ae57a548e
Bao Le <bao.l@kbtg.tech>
2019-07-25 15:55:55 +0700

    Add more routes

--- main.go
+++ main.go
@@ -10,10 +10,15 @@ func main() {
 	port := *flag.String("port", "7777", "Serve on port")
 
 	http.HandleFunc("/memo", showMemo)
+	http.HandleFunc("/memos", showMemos)
 	fmt.Println("Serve on port: ", port)
 	http.ListenAndServe(":" + port, nil)
 }
 
 func showMemo(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Memo"))
+}
+
+func showMemos(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("More memo"))
 }
