456f2ddf50e0fbbb8d62d54fba914c8b064080b8
Bao Le <bao.l@kbtg.tech>
2019-08-09 09:41:28 +0700

    add postman script

--- main.go
+++ main.go
@@ -6,6 +6,12 @@ import (
 	"flag"
 	"fmt"
 	"time"
+	"log"
+	"context"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type Memo struct {
@@ -16,11 +22,21 @@ type Memo struct {
 
 func main() {
 	port := *flag.String("port", "7777", "Serve on port")
+	// mongo
+	ctx, _ := context.WithTimeout(context.Background(), 10 * time.Second)
+	client, _ := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+	err := client.Ping(ctx, readpref.Primary())
+	if err != nil {
+		log.Panic("Cannot connected to the MongoDB.")
+	}
+	// -- mongo
+	fmt.Println("Serve on port: ", port)
+	log.Fatal(http.ListenAndServe(":" + port, nil))
+}
 
+func setupRouting() {
 	http.HandleFunc("/memo", showMemo)
 	http.HandleFunc("/memos", showMemos)
-	fmt.Println("Serve on port: ", port)
-	http.ListenAndServe(":" + port, nil)
 }
 
 func showMemo(w http.ResponseWriter, r *http.Request) {
